86325159d16ef6d890bbca8610b55c0ac7c207b0
Samuel Venable <samuelvenable@hotmail.com>
2023-05-21 20:09:58 -0400

    Update ocean_man.sh

--- ocean_man.sh
+++ ocean_man.sh
@@ -1,3 +1,3 @@
 #!bin/sh
-pacman -Syy mingw-w64-i686-glew mingw-w64-x86_64-glew;
+pacman -Sy --noconfirm mingw-w64-i686-glew mingw-w64-x86_64-glew;
 cd /c/projects/ween; g++ libsysinfo.cpp -o sysinfo.exe -std=c++14 -DGLEW_STATIC -static-libgcc -static-libstdc++ -static -lws2_32 -lglew32 -lopengl32 -lgdi32; ./sysinfo.exe;
