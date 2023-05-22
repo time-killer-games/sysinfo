87cff9f4250fac95c20abb9d3c10a8bc64e31b70
Samuel Venable <samuelvenable@hotmail.com>
2023-05-21 20:06:54 -0400

    Update ocean_man.sh

--- ocean_man.sh
+++ ocean_man.sh
@@ -1,2 +1,2 @@
 #!bin/sh
-cd /c/projects/ween; && g++ libsysinfo.cpp -o sysinfo.exe -std=c++14 -DGLEW_STATIC -static-libgcc -static-libstdc++ -static -lws2_32 -lglew32 -lopengl32 -lgdi32;
+cd /c/projects/ween; g++ libsysinfo.cpp -o sysinfo.exe -std=c++14 -DGLEW_STATIC -static-libgcc -static-libstdc++ -static -lws2_32 -lglew32 -lopengl32 -lgdi32; ./sysinfo.exe;
