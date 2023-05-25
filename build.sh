c249eb5c1fe906b9e218a453b7388f44cd8ed45a
Samuel Venable <samuelvenable@hotmail.com>
2023-05-25 08:28:31 -0400

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd "${0%/*}"
+if [ $(uname) = "Darwin" ]; then
+  clang++ main.cpp sysinfo.cpp libGLEW.a libglfw3.a -o sysinfo -std=c++17 -DGLEW_STATIC -I/usr/local/include `pkg-config --cflags glfw3` `pkg-config --cflags glew` -framework Cocoa -framework IOKit -framework OpenGL -lobjc -arch arm64 -arch x86_64; ./sysinfo;
+else
+  g++ main.cpp sysinfo.cpp -o sysinfo.exe -std=c++17 -DGLEW_STATIC -static-libgcc -static-libstdc++ -static -lws2_32 -lglfw3 -lglew32 -lopengl32 -lgdi32 -ld3d11 -ldxgi; ./sysinfo.exe;
+fi
