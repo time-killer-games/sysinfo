5cc1843b6e1f4fbebc0cf700976a36343f86834f
Samuel Venable <samuelvenable@hotmail.com>
2023-05-28 13:00:58 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@ cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
   clang++ main.cpp system.cpp libGLEW.a libglfw3.a -o sysinfo -std=c++17 -DGLEW_STATIC -Iinclude `pkg-config --cflags glfw3` `pkg-config --cflags glew` -framework Cocoa -framework IOKit -framework OpenGL -lobjc -arch arm64 -arch x86_64; ./sysinfo;
 elif [ $(uname) = "Linux" ]; then
-  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL; ./sysinfo;
+  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lEGL; ./sysinfo;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lkvm; ./sysinfo;
+  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lEGL -lkvm; ./sysinfo;
 elif [ $(uname) = "DragonFly" ]; then
-  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lkvm; ./sysinfo;
+  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lEGL -lkvm; ./sysinfo;
 elif [ $(uname) = "NetBSD" ]; then
-  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL; ./sysinfo;
+  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lEGL; ./sysinfo;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL; ./sysinfo;
+  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lEGL; ./sysinfo;
 elif [ $(uname) = "SunOS" ]; then
-  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL; ./sysinfo;
+  clang++ main.cpp system.cpp -o sysinfo -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lEGL; ./sysinfo;
 else
   g++ main.cpp system.cpp -o sysinfo.exe -std=c++17 -DGLEW_STATIC -static-libgcc -static-libstdc++ -static -lws2_32 -lglfw3 -lglew32 -lopengl32 -lgdi32 -ld3d11 -ldxgi; ./sysinfo.exe;
 fi
