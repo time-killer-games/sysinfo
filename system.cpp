20d9c196214a2035b2c8e8c6a3f16d6f61ba4972
Samuel Venable <samuelvenable@hotmail.com>
2024-02-14 17:38:22 -0800

    Add files via upload

--- system.cpp
+++ system.cpp
@@ -160,6 +160,9 @@ bool numcpuserror = false;
 long long totalram = -1;
 bool totalramerror = false;
 
+std::string isvirtual;
+bool isvirtualerror = false;
+
 #if defined(_WIN32)
 std::string wine_version;
 #endif
@@ -731,13 +734,20 @@ std::string os_architecture() {
 }
 
 std::string os_is_virtual() {
+  if (!isvirtual.empty())
+    return isvirtual;
+  if (isvirtualerror)
+    return pointer_null();
   #if (defined(__x86_64__) || defined(_M_X64) || defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86))
-  if (os_product_name().substr(0, 4) == "wine")
-    return "YES";
+  if (os_product_name().substr(0, 4) == "wine") {
+    isvirtual = "YES";
+    return isvirtual;
+  }
   #if (defined(__APPLE__) && defined(__MACH__))
-  if (os_architecture() == "x86_64" && 
-    read_output("sysctl -in sysctl.proc_translated") == "1")
-    return "YES";
+  if (read_output("sysctl -in sysctl.proc_translated") == "1") {
+    isvirtual = "YES";
+    return isvirtual;
+  }
   #endif
   class cpuid {
     #if defined(_MSC_VER)
@@ -781,8 +791,10 @@ std::string os_is_virtual() {
     }
     #endif
   };
-  if (!(cpuid(1, 0).ecx() & (1 << 31)))
-    return "NO";
+  if (!(cpuid(1, 0).ecx() & (1 << 31))) {
+    isvirtual = "NO";
+    return isvirtual;
+  }
   const int vendorIdLength = 13;
   using VendorIdStr = char[vendorIdLength];
   VendorIdStr hyperVendorId;
@@ -808,22 +820,31 @@ std::string os_is_virtual() {
     "VirtualApple"       // Newer versions of Apple Rosetta 2
   };
   for (const auto& vendor : vendors) {
-    if (!memcmp(vendor, hyperVendorId, vendorIdLength))
-      return "YES";
+    if (!memcmp(vendor, hyperVendorId, vendorIdLength)) {
+      isvirtual = "YES";
+      return isvirtual;
+    }
   }
-  return "NO";
+  isvirtual = "NO";
+  return isvirtual;
   #elif (defined(__APPLE__) && defined(__MACH__))
-  return "NO";
+  isvirtual = "NO";
+  return isvirtual;
   #elif defined(__linux__)
   std::string tmp = read_output("echo $(systemd-detect-virt 2> /dev/null)");
   std::transform(tmp.begin(), tmp.end(), tmp.begin(), ::toupper);
-  if (tmp.empty()) 
+  if (tmp.empty()) {
+    isvirtualerror = true;
     return pointer_null();
-  else if (tmp == "NONE")
-    return "NO";
-  return "YES";
+  } else if (tmp == "NONE") {
+    isvirtual = "NO";
+    return isvirtual;
+  }
+  isvirtual = "YES";
+  return isvirtual;
   #endif
   // non-x86 currently not supported outside of macOS and Linux ...
+  isvirtualerror = true;
   return pointer_null();
 }
 
