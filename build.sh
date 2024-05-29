fbab7820f05e041253534ee6a3dea4b5751f2a9e
Samuel Venable <samuelvenable@hotmail.com>
2024-05-29 01:57:07 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ xxd -i pci.ids > pci.ids.hpp;
 if [ $(uname) = "Darwin" ]; then
   clang++ main.cpp system.cpp -o sysinfo -std=c++17 -arch x86_64 -arch arm64; ./sysinfo -h 2> /dev/null;
 elif [ $(uname) = "Linux" ]; then
-  g++ main.cpp system.cpp -o sysinfo -std=c++17 -DCREATE_CONTEXT -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2` `pkg-config --cflags --libs x11` -lGL -lpthread; ./sysinfo -h 2> /dev/null;
+  g++ main.cpp system.cpp -o sysinfo -std=c++17 -DCREATE_CONTEXT -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2` `pkg-config --cflags --libs x11` -lGL -lhwloc -lpthread; ./sysinfo -h 2> /dev/null;
 elif [ $(uname) = "FreeBSD" ]; then
   clang++ main.cpp system.cpp -o sysinfo -std=c++17 -DCREATE_CONTEXT `pkg-config --cflags --libs sdl2` `pkg-config --cflags --libs x11` -lGL -lhwloc -lkvm -lpthread; ./sysinfo -h 2> /dev/null;
 elif [ $(uname) = "DragonFly" ]; then
@@ -14,7 +14,7 @@ elif [ $(uname) = "NetBSD" ]; then
 elif [ $(uname) = "OpenBSD" ]; then
   clang++ main.cpp system.cpp -o sysinfo -std=c++17 -DCREATE_CONTEXT `pkg-config --cflags --libs sdl2` `pkg-config --cflags --libs x11` -lGL -lhwloc -lpthread; ./sysinfo -h 2> /dev/null;
 elif [ $(uname) = "SunOS" ]; then
-  export PKG_CONFIG_PATH=/usr/lib/64/pkgconfig && g++ main.cpp system.cpp -o sysinfo -std=c++17 -DCREATE_CONTEXT -static-libgcc `pkg-config --cflags --libs sdl2` `pkg-config --cflags --libs x11` -lGL -lpthread; ./sysinfo -h 2> /dev/null;
+  export PKG_CONFIG_PATH=/usr/lib/64/pkgconfig && g++ main.cpp system.cpp -o sysinfo -std=c++17 -DCREATE_CONTEXT -static-libgcc `pkg-config --cflags --libs sdl2` `pkg-config --cflags --libs x11` -lGL -lhwloc -lpthread; ./sysinfo -h 2> /dev/null;
 else
-  g++ main.cpp system.cpp -o sysinfo.exe -std=c++17 -static-libgcc -static-libstdc++ -lws2_32 -ldxgi -lhwloc -lpsapi -static; ./sysinfo -h 2> /dev/null;
+  g++ main.cpp system.cpp -o sysinfo.exe -std=c++17 -static-libgcc -static-libstdc++ -lhwloc -lws2_32 -ldxgi -lpsapi -static; ./sysinfo -h 2> /dev/null;
 fi
